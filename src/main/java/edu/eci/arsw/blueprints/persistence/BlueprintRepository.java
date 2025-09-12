cd1af63217491759a0cc4fefeae72548413a182b
Esteban Aguilera <estebanac0903@gmail.com>
2025-09-12 07:49:33 -0500

    Adding testing for the principal classes

--- src/main/java/edu/eci/arsw/blueprints/persistence/BlueprintRepository.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/BlueprintRepository.java
@@ -1,10 +1,14 @@
 package edu.eci.arsw.blueprints.persistence;
 
-import edu.eci.arsw.blueprints.model.Blueprint;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
 
 public interface BlueprintRepository extends JpaRepository<Blueprint, Long> {
     Set<Blueprint> findByAuthor(String author);
     Blueprint findByAuthorAndName(String author, String name);
+
+    
 }
